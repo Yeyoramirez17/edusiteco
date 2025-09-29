a55592d5522c190713a61198ade8fc73ab3d472f
Yeison Ramirez <yeisonrazar98@gmail.com>
2025-09-29 16:32:05 -0500

    Config theme.json

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 function ita_enqueue_styles_scripts(): void {
     # CSS Normalize
-    wp_enqueue_style('ita_enqueue_css_normalize', get_theme_file_uri() . '/assets/css/normalize.css');
+    wp_enqueue_style('edusiteco_enqueue_css_normalize', get_theme_file_uri() . '/assets/css/normalize.css');
     # Tailwind CSS
+    wp_enqueue_script('edusiteco_enqueue_tailwindcss', "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4");
     # CSS
     wp_enqueue_style('edusiteco_enqueue_css_style', get_theme_file_uri() . '/assets/css/style.css');
     # JS
@@ -25,3 +26,24 @@ function edusiteco_add_editor_styles(): void {
     ]);
 }
 add_action('after_setup_theme', 'edusiteco_add_editor_styles');
+
+add_action( 'after_setup_theme', function() {
+    wp_enqueue_block_style(
+        'core/search',
+        array(
+            'handle' => 'core-search-custom',
+            'src'    => get_theme_file_uri( 'assets/css/blocks/search.css' ),
+            'path'   => get_theme_file_path('assets/css/blocks/search.css'),
+        )
+    );
+});
+
+add_filter( 'render_block_core/site-logo', function( $block_content, $block ) {
+    if ( empty( $block_content ) ) {
+        $default_logo = get_stylesheet_directory_uri() . '/assets/images/logoipsum.png';
+        $block_content = '<a href="' . esc_url( home_url( '/' ) ) . '" class="wp-block-site-logo__default">
+            <img src="' . esc_url( $default_logo ) . '" alt="' . get_bloginfo( 'name' ) . '">
+        </a>';
+    }
+    return $block_content;
+}, 10, 2 );
